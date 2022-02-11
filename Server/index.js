dbb461f4fb08113bfa65a09c491398dc1b252e5f
adeckvat <adeckvat.ru@mail.ru>
2022-02-11 15:09:31 +0300

    Adding db entity

---
+++ Server/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require("express")
+const models = require('./models/models')
+const sequelize = require('./db')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log('Server started on port', PORT))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+
+start()
